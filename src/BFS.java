c7eef6752cd41a4ccfab9fcec81de95c69f79481
Anusha <anushasekhar302002@gmail.com>
2024-12-21 12:39:47 +0530

    first commit

---
+++ src/BFS.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        // Add edges to the graph
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+        graph[5].add(new Edge(6, 5, 1));
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph, int V) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[V];
+
+        // Start BFS from node 0
+        q.add(0);
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!vis[curr]) {
+                System.out.print(curr + " ");
+                vis[curr] = true;
+
+                // Add neighbors to the queue
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        System.out.println("BFS Traversal:");
+        bfs(graph, V);
+    }
+}
